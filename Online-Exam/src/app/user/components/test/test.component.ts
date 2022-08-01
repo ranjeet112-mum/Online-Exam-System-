2ab586b36fbba912c5174a0a1259be276f37347a
ranjeet112-mum <ranjeet112pandey@gmail.com>
2022-08-01 21:25:48 +0530

    timer done

--- Online-Exam/src/app/user/components/test/test.component.ts
+++ Online-Exam/src/app/user/components/test/test.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
+import { count } from 'rxjs';
 import { UserService } from '../../user.service';
+
+// @ViewChild('hello', { static: false });
+ 
+
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
+
 export class TestComponent implements OnInit {
 
   Tests : any[] =[];
@@ -15,6 +21,7 @@ export class TestComponent implements OnInit {
   attemptId : any;
   testName : any;
   testduration: any;
+  // duration : any;
   name : any;
   questions :  any[] = [];
   question : any;
@@ -32,7 +39,14 @@ export class TestComponent implements OnInit {
   atLevel = 1;
   isCleared :any;
   timer : any;
-  constructor(private user : UserService,private route : Router) { }
+  divHello: any;
+  duration : any = 10;
+  interval : any;
+  timeout : any;
+  hh:number = 0;
+  mm:number = 0;
+  ss:number = 0;
+  constructor( private user : UserService,private route : Router) { }
 
   ngOnInit(): void {
     
@@ -43,7 +57,7 @@ export class TestComponent implements OnInit {
     }
 
     this.Tests = this.user.fetchAllTestsDummy(this.userId)
-    this.timer = document.getElementsByName('timer')[0];
+    this.timer = document.getElementsByName('timer')[0] || null;
     }
 
     displayTest(test : any){
@@ -73,21 +87,65 @@ export class TestComponent implements OnInit {
       this.question = this.questions[0];
       this.qindex = this.questions.indexOf(this.question) + 1;
       this.nextQuestion_id = this.questions[this.qindex].question_id;
+
     }
 
     showQuestion(){
       this.starttest = false;
       this.questionspage = true;
+      console.log(this.testduration);
+      
+      // console.log(this.timer);
+      // this.timer.innerText = "--:--"; 
+      
+      // this._elementRef.nativeElement.getElementById('timer').innerText = '__:__';
+      // console.log(this._elementRef.nativeElement.getElementById('timer'));
+      
       this.startTimer();
     }
 
+    
     startTimer(){
       console.log("timerstarted");
-      this.timer.innerText = '00:00';
-      
+      // this.timer.innerText = '00:00';
+      // count 
+      this.duration = Number(this.testduration); 
+      if(this.duration >= 60 ) {
+        for(;this.duration >= 60;){
+          this.duration -= 60;
+          this.hh++;
+        }
+      }
+      this.mm = this.duration-1;
+      this.ss = 60
+      this.interval = setInterval(()=>{
+        if(this.ss > 1 ) this.ss--;
+        else {
+          this.ss=60;
+          // this.mm--;
+          if(this.mm > 1 ) this.mm--;
+          else {
+            this.mm=60;
+            this.hh--;
+            
+          } 
+        }
+        
+        console.log(`${this.hh}:${this.mm}:${this.ss}`);
+        
+      },1000);
+
+      this.timeout = setTimeout(()=>{
+
+        this.submitTest(this.answers);
+        
+      },Number(this.duration) * 60 * 1000)
+
     }
 
     display(id : any ){
+      // if(this.duration <= 0 ){
+      // }
         this.questions.forEach(q => {if(q.question_id === id){
           this.question = q
         } });
@@ -115,6 +173,9 @@ export class TestComponent implements OnInit {
     }
 
     submitTest(answers : any){
+      clearInterval(this.interval);
+      clearTimeout(this.timeout);
+
       this.answers = answers;
       console.log(this.answers);
       //Todo send data from here to the back end for checking along with the attemptid
